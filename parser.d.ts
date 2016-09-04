2bbea2ee0c0ce8920d335762015c4247a2dc5127
Andrey Roenko <flapenguin@outlook.com>
2016-09-04 20:35:58 +0300

    Add dts for parser

---
+++ parser.d.ts
@@ -0,0 +1,37 @@
+interface Token<TokenType> {
+  type: TokenType
+}
+
+interface Expression {
+}
+
+interface Lexer<Token> {
+  consume(): Token;
+  skip(n?: number): void;
+  peek(n?: number): Token;
+}
+
+interface PrefixRuleSet<TokenType> {
+  [tokenType: string]: {
+    precedence: number;
+    parse(token: TokenType, parser: Parser<TokenType>, precedence: number): Expression;
+  };
+}
+
+interface PostfixRuleSet<TokenType> {
+  [tokenType: string]: {
+    precedence: number;
+    parse(token: TokenType, expr: Expression, parser: Parser<TokenType>, precedence: number): Expression;
+  };
+}
+
+interface ParserRules<TokenType> {
+  prefix: PrefixRuleSet<TokenType>;
+  postfix: PostfixRuleSet<TokenType>;
+}
+
+declare class Parser<TokenType> {
+  constructor(lexer: Lexer<Token<TokenType>>, rules: ParserRules<TokenType>);
+}
+
+export = Parser;
